27648262d95858856729bdc4b8db288a193d41da
Florian Delage <delageflo@ymail.com>
2021-01-14 13:56:36 +0100

    add MediaWiki.get_informations()

--- grand_py_app/apis/media_wiki.py
+++ grand_py_app/apis/media_wiki.py
@@ -4,5 +4,28 @@ import requests
 
 class MediaWiki():
 
-    def __init__(self):
-        pass
+    def __request_get(self, location):
+        payload = {
+            "action": "query",
+            "format": "json",
+            "generator": "geosearch",
+            "ggscoord": f"{location['lat']}|{location['lng']}",
+            "ggslimit": "10",
+            "ggsradius": "100",
+            "prop" : "extracts",
+            "exintro" : "",
+            "explaintext" : ""
+        }
+        request = requests.get("https://fr.wikipedia.org/w/api.php", params=payload)
+        return request.json()
+
+        def get_informations(self, location):
+            data = self.__request_get(location)
+            pages = data["query"]["pages"]
+            first_page = pages[list(pages)[0]]
+            location_infos = {
+                "title": first_page['title'],
+                "extract": first_page['extract']
+                "wiki_url": "https://fr.wikipedia.org/"
+            }
+            return location_infos
--- grand_py_app/views.py
+++ grand_py_app/views.py
@@ -1,7 +1,8 @@
 
 from flask import Flask, render_template, request, jsonify
 from grand_py_app.utils.question_cleaner import QuestionCleaner
-from grand_py_app.apis.google_maps import *
+from grand_py_app.apis.google_maps import GoogleMaps
+from grand_py_app.apis.media_wiki import MediaWiki
 
 
 app = Flask(__name__)
@@ -15,5 +16,7 @@ def process():
     question = request.form['question']
     cleaner = QuestionCleaner()
     maps = GoogleMaps()
+    wiki = MediaWiki()
     address = cleaner.address_for_url(question)
-    return jsonify({"location": maps.get_location(address)})
+    location = maps.get_location(address)
+    return jsonify({"data": wiki.get_informations(location)})
