21e8d6d73c1408eae33568d5ddd523f19e21ad6a
Florian Delage <delageflo@ymail.com>
2021-01-28 21:45:19 +0100

    update home.js

--- grand_py_app/static/js/home.js
+++ grand_py_app/static/js/home.js
@@ -1,8 +1,8 @@
 
 // Initialize and add the map
-function initMap(locationCoordinates, div) {
+function initMap(locationCoordinates, div, responsesDiv) {
 	const map = new google.maps.Map(document.getElementById(div), {
-	  zoom: 15,
+	  zoom: 17,
 	  center: locationCoordinates,
 	});
 	// The marker, positioned at location_coordinates
@@ -16,48 +16,65 @@ function initMap(locationCoordinates, div) {
 function userRequestSpeech(user_request) {
 	return "Je connais " + user_request + " !";
 }
-
-function titleSpeech(address) {
-	return "Je sais plein de choses sur " + address + " !";
+// --------------------
+function addressSpeech(address) {
+	return "Voici l'adresse: " + address + " !";
 }
 
+function responseAddress(address) {
+	const responseAddress = document.createElement("p");
+	responseAddress.className = "response-address";
+	responseAddress.textContent = addressSpeech(address);
+	return responseAddress;
+}
+// --------------------
 function extractSpeech(extract) {
-	return "Laisse moi te raconter... " + extract;
+	return "Je t'ai déjà parlé de ce quartier ? " + extract;
 }
 
+function responseExtract(extract) {
+	const responseExtract = document.createElement("p");
+	responseExtract.className = "response-extract";
+	responseExtract.textContent = extractSpeech(extract);
+	return responseExtract;
+}
+// --------------------
 function wikiSpeech() {
 	return "Si tu veux en savoir plus n'hésite pas à consulter cette page ";
 }
 
-function locationStory(address, extract, wiki_url) {
-	const responseElement = document.getElementById("response");
-
-	const responseAddress = document.createElement("p");
-	responseAddress.className = "response-address";
-	responseAddress.textContent = titleSpeech(address);
-	responseElement.appendChild(responseAddress);
-
-	const responseExtract = document.createElement("p");
-	responseExtract.className = "response-extract";
-	responseExtract.textContent = extractSpeech(extract);
-	responseElement.appendChild(responseExtract);
-
+function responseUrl(wiki_url) {
 	const responseUrl = document.createElement("p");
 	responseUrl.className = "response-url";
 	const urlLink = document.createElement("a");
 	urlLink.className = "url-link";
 	const urlLinkText = document.createTextNode("Wikipédia");
 	urlLink.setAttribute('href', wiki_url);
-	urlLink.setAttribute('target', '_blank')
+	urlLink.setAttribute('target', '_blank');
 	urlLink.appendChild(urlLinkText);
 	responseUrl.textContent = wikiSpeech();
 	responseUrl.appendChild(urlLink);
-	responseElement.appendChild(responseUrl);
+	return responseUrl;
+}
+
+function botResponses(address, extract, wiki_url, locationCoordinates, mapsDiv, responsesDiv) {
+	const responseElement = document.getElementById(responsesDiv);
+	const responseAddr = responseAddress(address);
+	const responseExtr = responseExtract(extract);
+	const responseLink = responseUrl(wiki_url);
+
+	responseElement.appendChild(responseAddr);
+	responseElement.appendChild(responseExtr);
+	responseElement.appendChild(responseLink);
+
+	initMap(locationCoordinates, mapsDiv);
 }
 
 // Ajax when user submits
 $(document).ready(function() {
-	$('#form-place').submit(function( event ) { //quand l utilisateur soumet sa saisie
+
+	$('#form-place').submit(function(event) { //quand l utilisateur soumet sa saisie
+
 		var place = $('#input_place').val(); //la saisie est stockée
 
 		$.ajax({
@@ -65,8 +82,12 @@ $(document).ready(function() {
 			data: $('form').serialize(), //la saisie est envoyée à la méthode de l'url /process
 			type: 'POST',
 			success: function(response) {
-				locationStory(response['infos']['maps_address'], response['infos']['wiki_extract'], response['infos']['wiki_url']);
-				initMap({'lat':response['infos']['maps_lat'],'lng':response['infos']['maps_lng']}, "map")
+				botResponses(address=response['infos']['maps_address'],
+							 extract=response['infos']['wiki_extract'],
+							 wiki_url=response['infos']['wiki_url'],
+							 locationCoordinates={'lat':response['infos']['maps_lat'],'lng':response['infos']['maps_lng']},
+							 mapsDiv="map",
+							 responsesDiv="response");
 			},
 			error: function(error) {
 				console.log(error);
