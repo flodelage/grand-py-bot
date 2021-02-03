dc2e9e6916bdb9cbb6751fcc99d040f23b982e15
Florian Delage <delageflo@ymail.com>
2021-02-03 10:30:53 +0100

    update app.config in run.py

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 
 from grand_py_app.views import app
-from env_variables import FLASK_KEY
+from env_variables import FLASK_KEY, MAPS_KEY
 
 
 app.config.update(
     DEBUG=True,
-    SECRET_KEY=FLASK_KEY
+    SECRET_KEY=FLASK_KEY,
+    MAPS_KEY=MAPS_KEY
 )
 
 if __name__ == "__main__":
