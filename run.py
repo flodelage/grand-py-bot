1b8e075360a8397020073fc0944a65fe88e88562
Florian Delage <delageflo@ymail.com>
2021-02-15 09:41:06 +0100

    heroku debug test

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 
 from grand_py_app.views import app
-from env_variables import FLASK_KEY, MAPS_KEY
+# from env_variables import FLASK_KEY, MAPS_KEY
 
 
 app.config.update(
     DEBUG=True,
-    SECRET_KEY=FLASK_KEY,
-    MAPS_KEY=MAPS_KEY
+    SECRET_KEY="854653653566536536363764876987080865752",
+    # MAPS_KEY=MAPS_KEY
 )
 
 if __name__ == "__main__":
