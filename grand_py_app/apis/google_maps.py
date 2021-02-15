68ee40f4f30d01ec45022a288d231f26811c35c8
Florian Delage <delageflo@ymail.com>
2021-02-15 09:33:45 +0100

    heroku debug test

--- grand_py_app/apis/google_maps.py
+++ grand_py_app/apis/google_maps.py
@@ -1,13 +1,14 @@
 
 import requests
 
-from env_variables import MAPS_KEY
+# from env_variables import MAPS_KEY
 
 
 class GoogleMaps():
 
     def __init__(self):
-        self.key = MAPS_KEY
+        # self.key = MAPS_KEY
+        self.key = "AIzaSyAltnzowbiyt85fq95bSJEZXcg93hgR8vA"
 
     def request_get(self, address):
         address_splited = address.split() # ['55', 'rue', 'faubourg', 'saint', 'honoré', 'paris']
