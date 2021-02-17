c84503359d5bc6b38b5f1537ee8575581549a63c
Florian Delage <delageflo@ymail.com>
2021-02-17 14:27:28 +0100

    .gitignore fix

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ load_dotenv()
 MAPS_GEOCODING_KEY = os.getenv("maps_geocoding_key")
 MAPS_JS_KEY = os.getenv("maps_js_key")
 FLASK_KEY = os.getenv("flask_key")
+
+MAPS_GEOCODING_KEY = os.environ("MAPS_GEOCODING_KEY")
+MAPS_JS_KEY = os.environ("MAPS_GEOCODING_KEY")
+FLASK_KEY = os.environ("MAPS_GEOCODING_KEY")
